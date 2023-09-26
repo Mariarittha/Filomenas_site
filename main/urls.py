3120399094836bdf495f33293b834d286397c610
Mariarittha <Mariaritha91@gmailcom>
2023-09-26 09:40:50 -0300

    templates/base

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from Filomenas.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
+    path('', index, name='index')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ templates/menu.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("scroll",function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem',window.scrollY > 0)
+
+})
+
+// window.addEventListener("scroll", function(){
+//     let header = document.querySelector('#header')
+//     header.classList.toggle('rolagem',window.scrollY > 0)
+// })
