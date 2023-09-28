2a35fc545e1a7829776e9681ffd3b8dc12fd5e48
Mariarittha <Mariaritha91@gmailcom>
2023-09-28 09:06:19 -0300

    atualização..

--- Filomenas/views.py
+++ Filomenas/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+# Sem cadastro
+
 def index(request):
     return render (request, 'Filomenas/index.html')
+
+
+# Com login
+
+def index_logado(request):
+    return render (request, 'Logado/index_logado.html')
+
+def Minha_conta (request):
+    return render (request, 'Logado/minha_conta.html')
--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from Filomenas.views import index
+from Filomenas.views import index, index_logado, Minha_conta
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('index/', index_logado, name='index_logado'),
+    path('minha_conta/', Minha_conta, name='minha_conta')
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
